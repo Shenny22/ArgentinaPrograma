a75f3efea8b9ec2e18281d97781ee16594b96763
shenny22 <jennyg1526@hotmail.com>
2023-05-24 01:47:53 -0300

    inciso a)

---
+++ multiplicacion.js
@@ -0,0 +1,8 @@
+// Inciso b)
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let multiplicacion = num1 * num2 * num3;
+console.log(multiplicacion);
+//¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+// Respuesta: Devuelve el número 204
