d8f0e523c013f9a74fcd50b29df43d6148869211
shenny22 <jennyg1526@hotmail.com>
2023-05-24 23:43:39 -0300

    1a)

---
+++ Practico0/resta.js
@@ -0,0 +1,9 @@
+// Ejercicio 1)
+// Inciso a)
+let minuendo=10;
+let sustraendo=2;
+let resta = minuendo - sustraendo;
+console.log(resta);
+
+// ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo es 2?
+// Respuesta: Devuelve el número 8 
