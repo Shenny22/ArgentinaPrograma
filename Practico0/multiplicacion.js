1a6ba41d62ff93fd6a8f4a194d394e331d86456a
shenny22 <jennyg1526@hotmail.com>
2023-05-24 23:49:33 -0300

    1b)

--- Practico0/multiplicacion.js
+++ Practico0/multiplicacion.js
@@ -1,9 +1,9 @@
 // Ejercicio 1)
-// Inciso b)
+// Inciso b) ¿Qué valor devuelve su programa si los números son 2 *17 * 6?
 let num1 = 2;
 let num2 = 17;
 let num3 = 6;
 let multiplicacion = num1 * num2 * num3;
 console.log(multiplicacion);
-//¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+
 // Respuesta: Devuelve el número 204
