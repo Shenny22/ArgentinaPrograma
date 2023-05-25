996958139b3573a8ae2d33d43d96da4c7462e942
shenny22 <jennyg1526@hotmail.com>
2023-05-25 01:14:17 -0300

    Practico N0 finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,16 @@
+// Ejercicio 2)
+// Inciso a) ¿Cuál es el área de un rectángulo de base 5cm y altura 3cm?
+
+let base =  5;
+let altura= 3;
+let area = base * altura;
+console.log(area);
+// Respuesta: el área es 15 cm**2
+
+// Inciso b) ¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm?
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log(area);
+
+// Respuesta: el área es 66.25 cm**2
---
+++ Practico0/promedio3.js
@@ -0,0 +1,24 @@
+// Ejercicio 4)
+//Inciso a) ¿Cuál es el promedio entre 5, 7 y 9?
+let x = 5; 
+let y = 7;
+let z = 9;
+let promedio = (x + y + z)/ 3;
+console.log(promedio);
+//Respuesta: El promedio es 7
+
+// Inciso b) ¿Cuál es el promedio entre -2 , -3, -8?
+x = -2;
+y = -3;
+z = -8;
+promedio = (x + y + z)/ 3;
+console.log(promedio);
+//Respuesta: El promedio es -4.33
+
+// Inciso c) ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x + y + z)/ 3;
+console.log(promedio);
+//Respuesta: El promedio es 0.266
