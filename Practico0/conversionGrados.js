2629a0c3757ee0e04445f284d01e89881068964d
shenny22 <jennyg1526@hotmail.com>
2023-05-25 00:40:09 -0300

    Agrego el archivo conversionGrados.js

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,14 @@
+// Ejercicio 3)
+//Inciso a) ¿Cuántos grados C son 50 F?
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * (5/9);
+console.log(celsius);
+
+// Respuesta: 50°F son 10°C
+
+//Inciso b) ¿Cuántos grados C son 75 F?
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * (5/9);
+console.log(celsius);
+
+// Respuesta: 75°F son 23.89°C
