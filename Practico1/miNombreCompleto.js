9f573fd5f10d007843d0db80a45508fa021bf039
Shenny22 <jennyg1526@hotmail.com>
2023-06-10 16:47:49 -0300

    PARTE1-Inciso1)

--- Practico1/miNombreCompleto.js
+++ Practico1/miNombreCompleto.js
@@ -1,5 +1,5 @@
 let nombre;
-nombre = 'Jennifer'
-let apellido = 'Godoy'
+nombre = "Jennifer"
+let apellido = "Godoy"
 let edad = 31
-console.log ('Hola,' + nombre + ' '+ apellido + '.Tienes '+ edad + ' años.');
+console.log ("Hola," + nombre + " "+ apellido +  ".Tienes "+ edad + " años.");
