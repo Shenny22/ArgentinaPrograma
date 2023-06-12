cae64025103ccade6f1609aaff110af7c2d906a5
Shenny22 <jennyg1526@hotmail.com>
2023-06-12 01:34:31 -0300

    PARTE2-Inciso5)

---
+++ Practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas = ["kiwi", "pera", "damasco", "frambuesa", "arandano"];
+let ultima = frutas[4];
+console.log("La última fruta agregada fue: "+ultima);
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: "+primera);
