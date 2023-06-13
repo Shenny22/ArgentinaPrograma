4703c05593c556f37e4e12e312b132ac63fc00e4
Shenny22 <jennyg1526@hotmail.com>
2023-06-12 21:58:57 -0300

    PARTE2-Inciso3)

--- Practico1/operacionesAritmeticas.js
+++ Practico1/operacionesAritmeticas.js
@@ -1,6 +1,6 @@
-let num1 = 18, num2 = -14, num3 = 11, num4=22;
-let suma = num1 + num2;
-let resta = num4 - num3;
+let var1 = 18, var2 = -14, var3 = 11, var4=22;
+let suma = var1 + var2;
+let resta = var4 - var3;
 let resultadoFinal = suma * resta;
 let esPar = (resultadoFinal % 2) == 0; 
-console.log ("Mis variables iniciales fueron: "+num1+", "+num2+", "+num3+" y "+num4+". La respuesta a verificar si el resultado final es par es: "+esPar);
+console.log ("Mis variables iniciales fueron: "+var1+", "+var2+", "+var3+" y "+var4+". La respuesta a verificar si el resultado final es par es: "+esPar);
