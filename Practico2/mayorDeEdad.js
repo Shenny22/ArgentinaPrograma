066381051167c370bc5b520ed2302b7b20482e88
Shenny22 <jennyg1526@hotmail.com>
2023-06-22 18:16:33 -0300

    Practico2

---
+++ Practico2/mayorDeEdad.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+const edad = readline.question("Ingrese su edad: ");
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
