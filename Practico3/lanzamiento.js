5f968fd75df857d436caa3ea356d1c2efd78c5b2
Shenny22 <jennyg1526@hotmail.com>
2023-06-27 20:23:07 -0300

    CuentaRegresiva

--- Practico3/lanzamiento.js
+++ Practico3/lanzamiento.js
@@ -1,7 +1,9 @@
 for (let i = 10; i >= 0 ; i--){
     if ( i === 10){
         console.log("Cuenta regresiva 10");
-    } else if (i === 0)
+    } else if (i === 0){
     console.log("Lanzamiento!");
-    else (console.log(i));
+    } else{
+        console.log(i);
+    }
 }
